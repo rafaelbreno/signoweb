70b0f06a9dd5480f9b5491737aaec97947e73685
rafaelbreno <(96b0963d)>
2020-05-11 15:06:39 -0300

    Created default CRUDController, just with some messages to test the Router

---
+++ App/Controllers/CRUDController.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Controllers;
+
+
+class CRUDController
+{
+    public function index()
+    {
+        echo "CRUDController@index";
+    }
+    public function create()
+    {
+        echo "CRUDController@create";
+    }
+    public function store()
+    {
+        echo "CRUDController@store";
+    }
+    public function update()
+    {
+        echo "CRUDController@update";
+    }
+    public function delete()
+    {
+        echo "CRUDController@delete";
+    }
+}
